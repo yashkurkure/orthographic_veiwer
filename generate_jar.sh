eceb9d47f9c2ba20ecf54d003ea49f725cce7006
yashkurkure <yash.kurkure1903@gmail.com>
2022-12-30 18:35:07 -0500

    Updates to jar generation script

--- generate_jar.sh
+++ generate_jar.sh
@@ -1,3 +1,3 @@
 mvn clean package
 cp target/orthographic_veiwer-1.0-SNAPSHOT-jar-with-dependencies.jar ./
-mv orthographic_veiwer-1.0-SNAPSHOT-jar-with-dependencies.jar orthographic_veiwer.jar
+mv orthographic_veiwer-1.0-SNAPSHOT-jar-with-dependencies.jar orthographic_viewer.jar
